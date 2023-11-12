987031f14312fc4c521d0e811802e35bafcdd9c6
Marek Posturzyński <marekposturzynski@gmail.com>
2023-11-12 11:25:38 +0100

    Main.java boolean practise

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
+        boolean isCold = true;
+        if(isCold) {
+            System.out. println("Oh it's cold wear a coat");
+        }
     }
 }
